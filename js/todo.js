e77a8aa47f54c2f21d3e28d311fa40405ef05436
byunjinseon <wachmaninoff@gmail.com>
2021-10-03 15:09:37 +0900

    css3

--- js/todo.js
+++ js/todo.js
@@ -23,7 +23,7 @@ const paintToDo = (newToDo) => {
   const span = document.createElement("span");
   const btn = document.createElement("button");
   let text = newToDo.text;
-  text = text.length < 14 ? text : text.substr(0, 14) + "...";
+  text = text.length < 11 ? text : text.substr(0, 11) + "...";
   span.innerText = text;
   btn.innerText = "❎";
   btn.addEventListener("click", deleteToDo);
